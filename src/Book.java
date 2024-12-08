5224d218031c056459d5c5628ab4de08122799e1
Vedika365 <vedikajain873@yahoo.com>
2024-12-08 16:21:12 -0500

    commit

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,4 @@
+
 public class Book {
     private String title;
     private String author;
@@ -48,7 +49,62 @@ public class Book {
         }
         return -1; //it means invalid ISBN
    }
-    
+
+   //convert Case methods to convert title and author to title case
+    public void toTitleCase( ) {
+        this.title = toTitleCase(this.title);
+        this.author = toTitleCase(this.author);
+
+    }
+    //TOString method
+    public Stirng toString() {
+        return "Title :" + title + "\n" +
+                "Author:" + author + "\n" +
+                "Price :" + String.format( % .2f,price)+"\n" +
+                "Publisher :" + publisher + " \n" +
+                "ISBN :" + isbn;
+    }
+    //equals method
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+
+        Book other = (Book) obj;
+        return Double.equals(other.price, price) == 0 &&
+                title.equals(other.title) &&
+                author.equals(other.author) &&
+                publisher.equals(other.publisher) &&
+                isbn.equals(other.isbn);
+    }
+
+    //clone method
+    public Book clone() {
+        return new Book(this);
+    }
+     //Getter and setters
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = toTitleCase(title);
+    }
+    public String getAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+    public String getPublisher(){
+        return publisher;
+    }
+    public void setPublisher(String publisher){
+        this.publisher = publisher;
+    }
+    public void setIsbn (String isbn){
+        this.isbn = isbn;
+    }
+
 
 
 
